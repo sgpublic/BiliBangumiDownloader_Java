dc262f19de2cbd894c57790c84b029798491f117
SGPublic <3300633844@qq.com>
2020-09-19 09:52:44 +0800

    200905

--- app/src/main/java/com/sgpublic/bilidownload/BangumiAPI/EpisodeHelper.java
+++ app/src/main/java/com/sgpublic/bilidownload/BangumiAPI/EpisodeHelper.java
@@ -1,6 +1,7 @@
 package com.sgpublic.bilidownload.BangumiAPI;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.sgpublic.bilidownload.DataHelper.Episode.DASHDownloadData;
 import com.sgpublic.bilidownload.DataHelper.Episode.FLVDownloadData;
